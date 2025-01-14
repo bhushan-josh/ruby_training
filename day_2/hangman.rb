e9a09a0e7795193e1cc351e8dfe8744735ca2683
bhushan-josh <bhushan.nagpure@joshsoftware.com>
2025-01-14 23:17:09 +0530

    updated logic

--- day_2/hangman.rb
+++ day_2/hangman.rb
@@ -1,4 +1,4 @@
-    
+
 def hangman
 
     hint = ['Body Part','Profession','Animal','Movie', 'Fruit']
@@ -8,41 +8,44 @@ def hangman
     level = 0
 
     loop do
-            print "\n------------------------------------ HangMan ------------------------------------\n"
-            print "\n\n"
-            print "Guess the word by help of given hint, You have 5 chances".center(80)
-            print "\n\n"
-            print ("Level : "+(level+1).to_s).center(80)
-            print "\n\n"
-            puts "---------------------------------------------------------------------------------"
-            print "\n"
-            print ("Hint => "+hint[level]).center(80)
-            print "\n\n"
-            puts "---------------------------------------------------------------------------------"
-            print "\n\n"
-
-
-            print ("["+ display_string[level] +"]").center(80)
-            print "\n"
-
+        
             while chances > 0 and word_to_guess[level] != display_string[level]
+                print "\n"
+                print "------------------------------------ HangMan ------------------------------------\n"
+                print "\n\n"
+                print "Guess the word by help of given hint, You have 5 chances".center(80)
+                print "\n\n"
+                print (("Level : "+(level+1).to_s) +"\t Chances : " + chances.to_s).center(80)
+                print "\n\n"
+                puts "---------------------------------------------------------------------------------"
+                print "\n"
+                print ("Hint => "+hint[level]).center(80)
+                print "\n\n"
+                puts "---------------------------------------------------------------------------------"
+                print "\n\n"
+        
+        
+                print ("["+ display_string[level] +"]").center(80)
+                print "\n"
 
                 user_input = gets.chomp
+                
                 
                 user_index = word_to_guess[level].index(user_input)
                 
                 if user_index
                     display_string[level][user_index] = user_input
+                    
                 else
                     chances -= 1
                     puts ("Wrong guess user remaing chances : "+chances.to_s).center(80)
+                    sleep(1)
                 end
-
-                print "\n"
-                print ("["+ display_string[level] +"]").center(80)
-                print "\n"
+                system("clear")
 
                 if word_to_guess[level] == display_string[level]
+                    print "\n"
+                    puts "---------------------------------------------------------------------------------"
                     print "\n"
                     print ("Congratulation level has been cleared").center(80)
                     print "\n\n"
@@ -50,9 +53,12 @@ def hangman
 
                 elsif chances == 0 and word_to_guess[level] != display_string[level]
                     print "\n"
-                    puts "You are dead, Try in next life".center(80)
+                    puts "---------------------------------------------------------------------------------"
+                    print "\n"
+                    puts "You are dead, Try again".center(80)
                     break
                 end
+
             end
 
             if chances == 0 and word_to_guess[level] != display_string[level]
@@ -65,13 +71,14 @@ def hangman
                 print "\n-------------------- You had successfully completed the Game --------------------\n"
                 break
             end
-            sleep(1)
+            sleep(2)
             system("clear")
     end
 
     print "\n----------------------------------- ©bhushan ------------------------------------\n"
-    puts    "---------------------------------------------------------------------------------\n\n\n\n"
+    puts    "---------------------------------------------------------------------------------"
 
 end
 
+system("clear")
 hangman
