1596fdce9e4e14446955149fbbc8914f8a4b8b40
bhushan-josh <bhushan.nagpure@joshsoftware.com>
2025-01-15 00:33:37 +0530

    indentation improvement

--- day_2/hangman.rb
+++ day_2/hangman.rb
@@ -1,82 +1,83 @@
-
 def hangman
+  hint = ['Body Part','Profession','Animal','Movie', 'Fruit']
+  word_to_guess = ['hand','pilot', 'dog', 'war','mango']
+  chances = 5
+  level = 0
 
-    hint = ['Body Part','Profession','Animal','Movie', 'Fruit']
-    word_to_guess = ['hand','pilot', 'dog', 'war','mango']
-    display_string = ['----','-----','---','---','-----']
-    chances = 5
-    level = 0
+  loop do
+    current_word = word_to_guess[level]
+    display_string = '-' * current_word.length
 
-    loop do
-        
-            while chances > 0 and word_to_guess[level] != display_string[level]
-                print "\n"
-                print "------------------------------------ HangMan ------------------------------------\n"
-                print "\n\n"
-                print "Guess the word by help of given hint, You have 5 chances".center(80)
-                print "\n\n"
-                print (("Level : "+(level+1).to_s) +"\t Chances : " + chances.to_s).center(80)
-                print "\n\n"
-                puts "---------------------------------------------------------------------------------"
-                print "\n"
-                print ("Hint => "+hint[level]).center(80)
-                print "\n\n"
-                puts "---------------------------------------------------------------------------------"
-                print "\n\n"
-        
-        
-                print ("["+ display_string[level] +"]").center(80)
-                print "\n"
+    while chances > 0 and current_word != display_string
+      print "\n"
+      print "------------------------------------ HangMan ------------------------------------\n"
+      print "\n\n"
+      print "Guess the word by help of given hint, You have 5 chances".center(80)
+      print "\n\n"
+      print ("Level : #{level + 1}\t Chances :  #{chances}").center(80)
+      print "\n\n"
+      puts "---------------------------------------------------------------------------------"
+      print "\n"
+      print ("Hint => #{hint[level]}").center(80)
+      print "\n\n"
+      puts "---------------------------------------------------------------------------------"
+      print "\n\n"
+      print ("[#{display_string}]").center(80)
+      print "\n"
 
-                user_input = gets.chomp
-                
-                
-                user_index = word_to_guess[level].index(user_input)
-                
-                if user_index
-                    display_string[level][user_index] = user_input
-                    
-                else
-                    chances -= 1
-                    puts ("Wrong guess user remaing chances : "+chances.to_s).center(80)
-                    sleep(1)
-                end
-                system("clear")
+      user_input = gets.chomp
+      
+      user_index = current_word.index(user_input)
+      
+      if user_index
+          display_string[user_index] = user_input
+      else
+          chances -= 1
+          puts ("Wrong guess user remaing chances : #{chances}").center(80)
+          sleep(1)
+      end
 
-                if word_to_guess[level] == display_string[level]
-                    print "\n"
-                    puts "---------------------------------------------------------------------------------"
-                    print "\n"
-                    print ("Congratulation level has been cleared").center(80)
-                    print "\n\n"
-                    puts "---------------------------------------------------------------------------------"
+      system("clear")
 
-                elsif chances == 0 and word_to_guess[level] != display_string[level]
-                    print "\n"
-                    puts "---------------------------------------------------------------------------------"
-                    print "\n"
-                    puts "You are dead, Try again".center(80)
-                    break
-                end
+      if current_word == display_string
+        print "\n"
+        puts "---------------------------------------------------------------------------------"
+        print "\n"
+        print ("Congratulation level #{level + 1} has been cleared!!!").center(80)
+        print "\n\n"
+        puts "---------------------------------------------------------------------------------"
+      elsif chances == 0 and current_word != display_string
+        print "\n"
+        puts "---------------------------------------------------------------------------------"
+        print "\n"
+        puts "You are dead, Try again!!!".center(80)
+        break
+      end
 
-            end
-
-            if chances == 0 and word_to_guess[level] != display_string[level]
-                break
-            end
-
-            chances = 5
-            level += 1
-            if level == 5
-                print "\n-------------------- You had successfully completed the Game --------------------\n"
-                break
-            end
-            sleep(2)
-            system("clear")
     end
 
-    print "\n----------------------------------- ©bhushan ------------------------------------\n"
-    puts    "---------------------------------------------------------------------------------"
+    # To exit game after all chances end.
+    if chances == 0 and current_word != display_string
+        break
+    end
+
+    chances = 5
+    level += 1
+
+    if level == 5
+      print "\n-------------------- You had successfully completed the Game --------------------\n"
+      break
+    end
+
+    sleep(2)
+    system("clear")
+  end
+
+  print "\n----------------------------------- ©bhushan ------------------------------------\n"
+  print "---------------------------------------------------------------------------------\n"
+
+  sleep(5)
+  system("clear")
 
 end
 
